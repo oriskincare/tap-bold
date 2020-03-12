30312150fddf8506f22845982bc978532122b304
oriskincare <ori.b@fabricskincare.com>
2020-03-12 14:41:48 +0200

    fixed setup.py

--- setup.py
+++ setup.py
@@ -2,23 +2,24 @@
 from setuptools import setup
 
 setup(
-    name="tap-bold",
-    version="0.1.0",
-    description="Singer.io tap for extracting data from Bold Recurring Orders Subscriptions API",
-    author="Ori Ben Aharon",
-    url="http://singer.io",
-    py_modules=["tap_bold"],
+    name='tap-bold',
+    version='0.1.0',
+    license='agpl-3.0',
+    description='Singer.io tap for extracting data from Bold Recurring Orders Subscriptions API',
+    author='Ori Ben Aharon',
+    url='http://singer.io',
+    py_modules=['tap_bold'],
     install_requires=[
-        "singer-python>=5.0.12",
-        "requests",
+        'singer-python>=5.0.12',
+        'requests',
     ],
     entry_points="""
     [console_scripts]
     tap-bold=tap_bold:main
     """,
-    packages=["tap_bold"],
-    package_data = {
-        "schemas": ["tap_bold/schemas/*.json"]
+    packages=['tap_bold'],
+    package_data={
+        'schemas': ['tap_bold/schemas/*.json']
     },
     keywords=['SINGER', 'TAP'],
     include_package_data=True,
@@ -26,10 +27,6 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'GNU GPLv3',  # Again, pick a license
-        'Programming Language :: Python :: 3',  # Specify which pyhton versions that you want to support
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3',
     ],
 )

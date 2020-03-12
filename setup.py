2b5604b3513ca78bbc92a1acaf6ae2e78d6e4083
oriskincare <ori.b@fabricskincare.com>
2020-03-12 14:35:22 +0200

    added download url

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setup(
     description="Singer.io tap for extracting data from Bold Recurring Orders Subscriptions API",
     author="Ori Ben Aharon",
     url="http://singer.io",
-    classifiers=["Programming Language :: Python :: 3 :: Only"],
     py_modules=["tap_bold"],
     install_requires=[
         "singer-python>=5.0.12",
@@ -21,5 +20,16 @@ setup(
     package_data = {
         "schemas": ["tap_bold/schemas/*.json"]
     },
+    keywords=['SINGER', 'TAP'],
     include_package_data=True,
+    download_url='https://github.com/oriskincare/tap-bold/archive/v0.1.0.tar.gz',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'GNU GPLv3',  # Again, pick a license
+        'Programming Language :: Python :: 3',  # Specify which pyhton versions that you want to support
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
 )
